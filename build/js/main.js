20378e5483d8b0e9c649951197e17b1c663cc735
Peter Grosskopf <peter.grosskopf@zweitag.de>
2013-08-05 10:39:51 +0200

    deploy

---
+++ build/js/main.js
@@ -0,0 +1,5 @@
+var map = L.mapbox.map('map', 'johannesboyne.map-nq2k0d15', {zoomControl: false});
+// map.dragging.disable();
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();

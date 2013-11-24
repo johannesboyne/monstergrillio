cf225d3a50a949bdfa412eadc2343d682e7e1dd5
Johannes Boyne <johannes@boyne.de>
2013-11-24 17:18:22 +0100

    Added the archive page and made middleman load it plus organized our scss code

--- config.rb
+++ config.rb
@@ -51,6 +51,8 @@ set :js_dir, 'js'
 
 set :images_dir, 'img'
 
+activate :directory_indexes
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment

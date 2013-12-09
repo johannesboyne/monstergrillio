1353e35f2a356a74ab2363da018aa19b17375527
Johannes Boyne <johannes@boyne.de>
2013-12-09 18:07:28 +0100

    Added List

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,15 @@
 var map = L.mapbox.map('map', 'johannesboyne.map-nq2k0d15', {zoomControl: false});
-// map.dragging.disable();
 map.touchZoom.disable();
 map.doubleClickZoom.disable();
 map.scrollWheelZoom.disable();
+
+var options = {valueNames: ['deliciousItem']};
+var dishesList = new List('dishes', options);
+
+$(function () {
+  $('#delSort').click(function () {
+    $(dishesList.items).each(function (i,e) { $(e.elm).removeClass('highlighted'); });
+    dishesList.sort('delicioItem', {sortFunction: function (a,b) { return Math.floor(Math.random()*10); }});
+    $(dishesList.items[0].elm).addClass('highlighted');
+  });
+});
